6abd4cdab8b11fa7ab133215bf5c47c42e94e417
Dushyant-singh <117542023+Dushyant-singh10@users.noreply.github.com>
2025-08-18 01:55:39 +0530

    Time: 0 ms (100%), Space: 42 MB (66.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-       int low=0;
-       int high=nums.length-1;
-       int mid=0;
-       while(mid<=high){
-        if(nums[mid]==0){
-            swap(nums,mid,low);
-                mid++;
-                low++;
-            }
-            else if(nums[mid]==1)
-            mid++;
-            else{
-                swap(nums,mid,high);
-                high--;
-            }
+        int largest = Integer.MIN_VALUE;
+        for(int i =0;i<nums.length;i++){   //here we find the max element
+            largest =Math.max(largest,nums[i]);
         }
-       }
-        public static void swap(int arr[],int a,int b){
-            int temp=arr[a];
-            arr[a]=arr[b];
-            arr[b]=temp;
+        int count [] = new int[largest+1]; // we have create a new array of count why we have done largest+1 here bexause o is also postive number
+
+        for(int i =0;i<nums.length;i++){
+            count[nums[i]]++;              //here we are storing freqency
+        }
+
+        int index = 0;
+        for(int i =0;i<count.length;i++){
+            while(count[i]>0){   //overwrite with sorted values
+                nums[index]= i;
+                index++;
+                count[i]--;
+            }
         }
     }
+}
